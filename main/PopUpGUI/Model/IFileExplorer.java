8db9bd0e76988e968c98d1b0e14704d0f4bc294a
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2022-09-11 15:59:59 +0200

    Remove useless access identifier in interface

--- main/PopUpGUI/Model/IFileExplorer.java
+++ main/PopUpGUI/Model/IFileExplorer.java
@@ -6,5 +6,5 @@ import main.Files.MyFile;
 import java.util.List;
 
 public interface IFileExplorer {
-    public List<MyFile> getSubFiles(MyFile file);
+    List<MyFile> getSubFiles(MyFile file);
 }
