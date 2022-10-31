06555936c41aa4dd4ffc5061cc26f0e54442d367
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2022-10-31 11:23:53 +0100

    Add MouseListener for menu items

---
+++ main/Frontend/Listeners/ItemMouseListener.java
@@ -0,0 +1,32 @@
+package main.Frontend.Listeners;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ItemMouseListener implements MouseListener {
+    private final Action _itemAction;
+
+    public ItemMouseListener(Action itemAction){
+        _itemAction = itemAction;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        var source = e.getSource();
+        var id = e.getID();
+        var actionEvent = new ActionEvent(source, id, null);
+
+        _itemAction.actionPerformed(actionEvent);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
+}
--- main/Frontend/UIBuilder.java
+++ main/Frontend/UIBuilder.java
@@ -1,19 +1,20 @@
 package main.Frontend;
 
-import main.Frontend.ItemActions.NewFile;
-import main.Frontend.ItemActions.OpenFile;
-import main.Frontend.ItemActions.SaveFile;
+import main.Frontend.Actions.*;
+import main.Frontend.Listeners.ItemMouseListener;
 import main.Frontend.Main.MainFrame;
 import main.Frontend.Main.MainPanel;
 
 import javax.swing.*;
 import java.awt.*;
 
-// Static class
+// TODO: refactor this:
+// TODO: add regions
+// TODO: factorize some code that is shared by a lot of subroutines
 public class UIBuilder {
 
     public static void build(){
-        Component header = getFileMenu();
+        Component header = getMenuBar();
         Component main = new JButton("Main");
         Component footer = new JButton("Footer");
 
@@ -21,14 +22,26 @@ public class UIBuilder {
         var mainFrame = new MainFrame(mainPanel);
     }
 
+    private static JMenuBar getMenuBar() {
+        JMenu fileMenu = getFileMenu();
+        JMenu editMenu = getEditMenu();
+        JMenu viewMenu = getViewMenu();
+
+        JMenuBar menuBar = new JMenuBar();
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(viewMenu);
+
+        return menuBar;
+    }
 
     private static JMenu getFileMenu(){
-        var newItem = getNewItem();
-        var openItem = getOpenItem();
-        var saveItem = getSaveItem();
+        JMenuItem newItem = getNewItem();
+        JMenuItem openItem = getOpenItem();
+        JMenuItem saveItem = getSaveItem();
 
-
-        var fileMenu = new JMenu();
+        var fileMenu = new JMenu("File");
 
         fileMenu.add(newItem);
         fileMenu.add(openItem);
@@ -38,50 +51,166 @@ public class UIBuilder {
 
     //TODO: refactor this, key and new should be properties
     private static JMenuItem getNewItem(){
+        var newMenuItem = new JMenuItem("New");
+
         var key = "meta N";
         var keyStroke = KeyStroke.getKeyStroke(key);
         var action = new NewFile();
-        var inputMap = new InputMap();
+        var inputMap = new ComponentInputMap(newMenuItem);
         var actionMap = new ActionMap();
         actionMap.put(key, action);
         inputMap.put(keyStroke, key);
 
-        var newMenuItem = new JMenuItem("New");
-        newMenuItem.setInputMap(JComponent.WHEN_FOCUSED, inputMap);
+        var newItemMouseListener = new ItemMouseListener(action);
+        newMenuItem.addMouseListener(newItemMouseListener);
+
+
+        newMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
         newMenuItem.setActionMap(actionMap);
+        newMenuItem.setAccelerator(keyStroke);
 
         return newMenuItem;
     }
 
     private static JMenuItem getOpenItem() {
+        var openMenuItem = new JMenuItem("Open");
+
         var key = "meta O";
         var keyStroke = KeyStroke.getKeyStroke(key);
         var action = new OpenFile();
-        var inputMap = new InputMap();
+        var inputMap = new ComponentInputMap(openMenuItem);
         var actionMap = new ActionMap();
         actionMap.put(key, action);
         inputMap.put(keyStroke, key);
 
-        var openMenuItem = new JMenuItem("Open");
-        openMenuItem.setInputMap(JComponent.WHEN_FOCUSED, inputMap);
+        var openItemMouseListener = new ItemMouseListener(action);
+        openMenuItem.addMouseListener(openItemMouseListener);
+
+        openMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
         openMenuItem.setActionMap(actionMap);
+        openMenuItem.setAccelerator(keyStroke);
 
         return openMenuItem;
     }
 
     private static JMenuItem getSaveItem(){
+        var saveMenuItem = new JMenuItem("Save");
+
         var key = "meta S";
         var keyStroke = KeyStroke.getKeyStroke(key);
         var action = new SaveFile();
-        var inputMap = new InputMap();
+        var inputMap = new ComponentInputMap(saveMenuItem);
         var actionMap = new ActionMap();
         actionMap.put(key, action);
         inputMap.put(keyStroke, key);
 
-        var saveMenuItem = new JMenuItem("Save");
-        saveMenuItem.setInputMap(JComponent.WHEN_FOCUSED, inputMap);
+        var saveItemMouseListener = new ItemMouseListener(action);
+        saveMenuItem.addMouseListener(saveItemMouseListener);
+
+        saveMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
         saveMenuItem.setActionMap(actionMap);
+        saveMenuItem.setAccelerator(keyStroke);
 
         return saveMenuItem;
+    }
+
+    private static JMenu getEditMenu() {
+        JMenuItem undoItem = getUndoMenuItem();
+        JMenuItem redoItem = getRedoMenuItem();
+        JMenuItem findItem = getFindMenuItem();
+        JMenuItem replaceItem = getReplaceMenuItem();
+
+        var editMenu = new JMenu("Edit");
+        editMenu.add(undoItem);
+        editMenu.add(redoItem);
+        editMenu.add(findItem);
+        editMenu.add(replaceItem);
+
+        return editMenu;
+    }
+
+    private static JMenuItem getUndoMenuItem() {
+        var undoMenuItem = new JMenuItem("Undo");
+
+        var key = "meta Z";
+        var keyStroke = KeyStroke.getKeyStroke(key);
+        var action = new UndoEdit();
+        var inputMap = new ComponentInputMap(undoMenuItem);
+        var actionMap = new ActionMap();
+        actionMap.put(key, action);
+        inputMap.put(keyStroke, key);
+
+        var undoMenuItemMouseListener = new ItemMouseListener(action);
+        undoMenuItem.addMouseListener(undoMenuItemMouseListener);
+
+        undoMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
+        undoMenuItem.setActionMap(actionMap);
+        undoMenuItem.setAccelerator(keyStroke);
+
+        return undoMenuItem;
+    }
+
+    private static JMenuItem getRedoMenuItem() {
+        var redoMenuItem = new JMenuItem("Redo");
+        var key = "meta Y";
+        var keyStroke = KeyStroke.getKeyStroke(key);
+        var action = new RedoEdit();
+        var inputMap = new ComponentInputMap(redoMenuItem);
+        var actionMap = new ActionMap();
+        actionMap.put(key, action);
+        inputMap.put(keyStroke, key);
+
+        var redoMenuItemMouseListener = new ItemMouseListener(action);
+        redoMenuItem.addMouseListener(redoMenuItemMouseListener);
+
+        redoMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
+        redoMenuItem.setActionMap(actionMap);
+        redoMenuItem.setAccelerator(keyStroke);
+
+        return redoMenuItem;
+    }
+
+    private static JMenuItem getFindMenuItem() {
+        var findMenuItem = new JMenuItem("Find");
+        var key = "meta F";
+        var keyStroke = KeyStroke.getKeyStroke(key);
+        var action = new FindEdit();
+        var inputMap = new ComponentInputMap(findMenuItem);
+        var actionMap = new ActionMap();
+        actionMap.put(key, action);
+        inputMap.put(keyStroke, key);
+
+        var findMenuItemMouseListener = new ItemMouseListener(action);
+        findMenuItem.addMouseListener(findMenuItemMouseListener);
+
+        findMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
+        findMenuItem.setActionMap(actionMap);
+        findMenuItem.setAccelerator(keyStroke);
+
+        return findMenuItem;
+    }
+
+    private static JMenuItem getReplaceMenuItem() {
+        var replaceMenuItem = new JMenuItem("Replace");
+        var key = "meta R";
+        var keyStroke = KeyStroke.getKeyStroke(key);
+        var action = new ReplaceEdit();
+        var inputMap = new ComponentInputMap(replaceMenuItem);
+        var actionMap = new ActionMap();
+        actionMap.put(key, action);
+        inputMap.put(keyStroke, key);
+
+        var replaceMenuItemMouseListener = new ItemMouseListener(action);
+        replaceMenuItem.addMouseListener(replaceMenuItemMouseListener);
+
+        replaceMenuItem.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, inputMap);
+        replaceMenuItem.setActionMap(actionMap);
+        replaceMenuItem.setAccelerator(keyStroke);
+
+        return replaceMenuItem;
+    }
+
+    private static JMenu getViewMenu() {
+        return new JMenu("View");
     }
 }
