152072d95e70c7f8f1bae95618043cc5028c5f39
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2022-11-12 22:11:08 +0100

    Clean main code a bit

--- main/Main.java
+++ main/Main.java
@@ -2,14 +2,10 @@ package main;
 import main.Frontend.Dialogs.PopUpDialogsBuilder;
 import main.Frontend.Main.MainFrameBuilder;
 
-import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args){
         MainFrameBuilder.build();
         PopUpDialogsBuilder.build();
-
-        var fileChooser = new JFileChooser();
     }
 }
